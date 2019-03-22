8e31e7825e62b40acdc7b15dd9a4e7b1108eb077
Willem Essenstam <wessenstam@gmail.com>
2019-03-22 16:33:29 +0100

    Updating the repository

--- TS2019/image_fix.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for i in `seq 1 113`;
-do
-        echo Connecting to 10.42.${i}.39
-        nuclei -server 10.42.${i}.39 -username admin -password techX2019! image.update VeeamBR_9.5.4.2615.Update4.iso image_type=ISO_IMAGE wait=false
-        nuclei -server 10.42.${i}.39 -username admin -password techX2019! image.update Nutanix-VirtIO-1.1.3.iso image_type=ISO_IMAGE wait=false
-        nuclei -server 10.42.${i}.39 -username admin -password techX2019! image.update Windows2012R2.iso image_type=ISO_IMAGE wait=false
-        nuclei -server 10.42.${i}.39 -username admin -password techX2019! image.update SQLServer2014SP3.iso image_type=ISO_IMAGE wait=false
-done
--- TS2019/logs_old/log_sim.sh
+++
@@ -1,8 +0,0 @@
-# Script to simulate the Clusters being installed
-./curl_sim.sh &
-sleep 5
-./curl_sim1.sh &
-sleep 5
-./curl_sim2.sh &
-sleep 5
-./curl_sim3.sh &
